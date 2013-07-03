ba8667e15561fcdbbdc8d3409bf7e8733b759e63
Vivin Paliath <vivin.paliath@gmail.com>
2013-07-03 15:20:01 -0700

    Fixing version number. Changes since 1.2.3 adds a lot of new features.

--- src/regula.js
+++ src/regula.js
@@ -1,7 +1,7 @@
 /**
  * @preserve
  * Regula: An annotation-based form-validation framework in Javascript
- * Version 1.2.4-SNAPSHOT
+ * Version 1.3.0-SNAPSHOT
  *
  * Written By Vivin Paliath (http://vivin.net)
  * License: BSD License
