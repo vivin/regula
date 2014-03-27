40010164a6427d4a0510164b52c7a1decbea7780
Vivin Paliath <vivin.paliath@gmail.com>
2014-03-27 11:21:46 -0700

    Hopefully this actually submits coverage data to coveralls.io

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,5 +73,5 @@ module.exports = function (grunt) {
     grunt.registerTask('test', ['bgShell:buildTestDistribution', 'bgShell:startAsyncTestServer', 'qunit_junit', 'qunit', 'bgShell:stopAsyncTestServer']);
     grunt.registerTask('build', ['bgShell:build']);
     grunt.registerTask('release', ['bgShell:buildRelease']);
-    grunt.registerTask('travis', ['test', 'coveralls']);
+    grunt.registerTask('travis', ['test']);
 };
