0b2ebaaab616244fedaa98879242f4323abbed1d
Vivin Paliath <vivin.paliath@gmail.com>
2014-07-24 15:43:43 -0700

    converting to one-column layout

--- js/documentation.js
+++ js/documentation.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function() {
+    /*
     var windowHeight = jQuery(window).height();
     var tocTopPosition = jQuery("#toc").position().top;
     var height = windowHeight - tocTopPosition - 100;
@@ -9,5 +10,5 @@ jQuery(document).ready(function() {
     if(window.location.pathname.indexOf("#") > -1) {
         var documentationElementid = window.location.pathname.replace(/^.*#/, "");
 
-    }
+    }*/
 });
